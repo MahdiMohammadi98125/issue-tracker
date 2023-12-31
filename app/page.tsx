d03dc40c5a6afcedce0d18323d31698a34d4e87c
MahdiMohammadi98125 <mahdi.mohammadi012021@gmail.com>
2023-12-31 16:49:33 +0430

    build issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma?.issue.count({ where: { status: "OPEN" } });
+  const closed = await prisma?.issue.count({ where: { status: "CLOSED" } });
+  const inProgress = await prisma?.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
   return (
     <div>
-      <LatestIssues />
+      <IssueSummary open={open!} closed={closed!} inProgress={inProgress!} />
     </div>
   );
 }
